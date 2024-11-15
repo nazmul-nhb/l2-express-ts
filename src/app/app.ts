eac9079c6fdd335520226cbf5e9d994a39ba3c76
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-15 13:23:23 +0600

    explored middlewares and parsers

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,26 @@
 import express from "express";
-import type { Request, Express, Response } from "express";
+import type { Request, Express, Response, NextFunction } from "express";
 
 const app: Express = express();
 
-app.use(express.json())
-app.use(express.text())
+app.use(express.json());
+app.use(express.text());
 
-app.get("/", (_req: Request, res: Response) => {
+const logger = (req: Request, _res: Response, next: NextFunction) => {
+	console.log(req.headers, req.hostname);
+	next();
+};
+
+app.get("/:id/:subId", logger, (req: Request, res: Response) => {
+	console.log(req.params);
+	console.log(req.query);
 	res.send("Hello Express!");
 });
 
-app.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
+app.post("/", (req: Request, res: Response) => {
+	console.log(req.body);
 
-    res.send("Received Content!")
-})
+	res.send("Received Content!");
+});
 
 export default app;
