90f14e43279dbdd26f094e888486fdb30f2468f5
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-15 13:47:43 +0600

    explored routes and error handlers

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,6 +11,33 @@ const logger = (req: Request, _res: Response, next: NextFunction) => {
 	next();
 };
 
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+	const user = req.body;
+	console.log(user);
+
+	res.json({
+		success: true,
+		message: "User is created successfully",
+		data: user,
+	});
+});
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+	const course = req.body;
+	console.log(course);
+	res.json({
+		success: true,
+		message: "User is created successfully",
+		data: course,
+	});
+});
+
 app.get("/:id/:subId", logger, (req: Request, res: Response) => {
 	console.log(req.params);
 	console.log(req.query);
@@ -21,6 +48,41 @@ app.post("/", (req: Request, res: Response) => {
 	console.log(req.body);
 
 	res.send("Received Content!");
+});
+
+app.get(
+	"/",
+	logger,
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			let mistake: undefined;
+			res.send(mistake);
+		} catch (error) {
+			next(error);
+			// res.status(400).json({
+			//   success: false,
+			//   message: "failed to get data",
+			// });
+		}
+	}
+);
+
+// handle 404
+app.all("*", (req: Request, res: Response) => {
+	res.status(400).json({
+		success: false,
+		message: "Route is not found",
+	});
+});
+
+// global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+	if (error) {
+		res.status(400).json({
+			success: false,
+			message: "Something went wrong",
+		});
+	}
 });
 
 export default app;
